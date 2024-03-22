5fe3cffc5cc8ba275c389d5026b6c4ca6a7beaa2
unknown <lalitha.putsala@divami.com>
2024-03-23 00:13:58 +0530

    data products-card component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './Sass/Variable.scss';
+import {AllCards}  from './Components/AllCards/AllCards.jsx';
 function App() {
     console.log(process.env.REACT_APP_API_URL)
     return (
-        <h1>Hai</h1>
+        <AllCards/>
+        
     );
 }
 
