3b0b4a63a0ec4eebaa1872579f2e76a5c674cd24
saikoteswararao <saikoteswararao@divami.com>
2024-03-23 01:10:09 +0530

    Job description code reverted

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './Sass/Variable.scss';
 import FormPage from './Pages/FormPage/FormPage';
 import Header from './Components/Common/Header/Header';
 import Welcome from './Components/Welcome/Welcome';
+import JobDescription from './Components/Common/JobDescription/JobDescription';
 function App() {
     console.log(process.env.REACT_APP_API_URL)
     return (
         <>
         <Header/>
         <FormPage/>
+        <JobDescription />
         </>
     );
 }
