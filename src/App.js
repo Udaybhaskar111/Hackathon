dcf91f4622b51b22c5b763676b9577999e8869c0
unknown <charan.maraboina@divami.com>
2024-03-23 04:28:07 +0530

    TabSwitch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './Sass/Variable.scss';
 import Home from './Pages/Home/Home';
+<<<<<<< Updated upstream
 import List from './Pages/ListPage/List'
+=======
+import TabSwitch from './Components/Common/TabSwitch/TabSwitch';
+
+>>>>>>> Stashed changes
 function App() {
     console.log(process.env.REACT_APP_API_URL)
     return (
         <>
-            <Home/>
+            {/* <Home/>
+             */}
+             <TabSwitch/>
         </>
     );
 }
