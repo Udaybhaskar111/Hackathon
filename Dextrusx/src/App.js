80357f8a15acf72a26529693602f21748b09aa24
unknown <lalitha.putsala@divami.com>
2024-03-22 19:29:42 +0530

    boiler plate

---
+++ Dextrusx/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Sass/Variable.scss';
+function App() {
+    console.log(process.env.REACT_APP_API_URL)
+    return (
+        <h1>Hai</h1>
+    );
+}
+
+export default App;
