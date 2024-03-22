f68b7712607c16ffe1db238adb61bfc066c2b393
saikoteswararao <saikoteswararao@divami.com>
2024-03-23 01:10:09 +0530

    Job-description

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './Sass/Variable.scss';
+import JobDescription from './Components/Common/JobDescription/JobDescription';
 function App() {
     console.log(process.env.REACT_APP_API_URL)
     return (
-        <h1>Hai</h1>
+        <div>
+            <JobDescription />
+        </div>
     );
 }
 
---
+++ src/Components/Common/JobDescription/JobDescription.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { data } from './Data'
+import './JobDescription.scss'
+
+const JobDescription = () => {
+
+   
+
+    return (
+        <div>
+            {data.map((e)=>(
+                <div key={e.id} className='Data'>
+                    <ul className='Data__List'>
+                        <li className='Data__List__Heading'> <h1>{e.heading}</h1></li>    
+                        
+                    </ul>
+                </div>
+               
+            ))}
+        </div>
+        
+    )
+}
+
+export default JobDescription
