e2b565049463858e98138c54df7912070937dd5d
unknown <charan.maraboina@divami.com>
2024-03-22 23:52:44 +0530

    welcome top part

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './Sass/Variable.scss';
+import Welcome from './Components/Welcome/Welcome';
 function App() {
-    console.log(process.env.REACT_APP_API_URL)
-    return (
-        <h1>Hai</h1>
-    );
+  console.log(process.env.REACT_APP_API_URL);
+  return (
+    <div>
+      <Welcome></Welcome>
+    </div>
+  );
 }
 
 export default App;
