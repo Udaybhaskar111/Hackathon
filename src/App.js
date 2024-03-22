df9830e05a43ecec4560a1ef69f91308c1e2f8e0
UdaybhaskarTalari <20a91a1252@aec.edu.in>
2024-03-22 23:51:57 +0530

    form page set up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './Sass/Variable.scss';
+import FormPage from './Pages/FormPage/FormPage';
 function App() {
     console.log(process.env.REACT_APP_API_URL)
     return (
+        <>
         <h1>Hai</h1>
+        <FormPage/>
+        </>
     );
 }
 
