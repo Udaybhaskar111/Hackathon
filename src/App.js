a551b2a5f208b3121fecff0f051491f541754a9e
greeshmika5 <greeshmika.thadwai@divami.com>
2024-03-23 04:10:22 +0530

    created list component to show data in list view

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Sass/Variable.scss';
 import Home from './Pages/Home/Home';
-
+import List from './Pages/ListPage/List'
 function App() {
     console.log(process.env.REACT_APP_API_URL)
     return (
