5726e0237546ed74662eadc1ab7fb19e19abc262
greeshmika5 <greeshmika.thadwai@divami.com>
2024-03-23 04:48:24 +0530

    resolved the conflicts

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import './Sass/Variable.scss';
 import Home from './Pages/Home/Home';
-<<<<<<< Updated upstream
-import List from './Pages/ListPage/List'
-=======
-import TabSwitch from './Components/Common/TabSwitch/TabSwitch';
 
->>>>>>> Stashed changes
+
 function App() {
     console.log(process.env.REACT_APP_API_URL)
     return (
         <>
-            {/* <Home/>
-             */}
-             <TabSwitch/>
+            <Home/>
         </>
     );
 }
